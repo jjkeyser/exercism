3859b25ffe0574a029d4c125b031b7f465415bf8
Jake Keyser <jakob.keyser@gmail.com>
2019-11-12 09:45:17 -0700

    added raindrops directory and solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,19 @@
 def convert(number):
-    pass
+    sound = ""
+    if number % 3 == 0 and number % 5 == 0 and number % 7 == 0:
+        sound = "PlingPlangPlong"
+    elif number % 3 == 0 and number % 5 == 0:
+        sound = "PlingPlang"
+    elif number % 3 == 0 and number % 7 == 0:
+        sound = "PlingPlong"
+    elif number % 3 == 0:
+        sound = "Pling"
+    elif number % 5 == 0 and number % 7 == 0:
+        sound = "PlangPlong"
+    elif number % 5 == 0:
+        sound = "Plang"
+    elif number % 7 == 0:
+        sound = "Plong"
+    else:
+        sound = str(number)
+    return sound
