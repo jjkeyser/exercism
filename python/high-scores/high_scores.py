8e7d0dcdf398351797986fcef270268ac67c766e
Jake Keyser <jakob.keyser@gmail.com>
2019-12-14 15:45:47 -0700

    fixed missing return value

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -7,7 +7,7 @@ def personal_best(scores):
 def personal_top_three(scores):
     #scores_list= scores
     #scores_list.sort(reverse = True)
-    print(sorted(scores, reverse= True)[:3])
-    #return scores_list[:3]
+    #print(sorted(scores, reverse= True)[:3])
+    return sorted(scores, reverse= True)[:3]
 
 personal_top_three([1,13])
