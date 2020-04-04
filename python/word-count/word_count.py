43fa3016a08e4c076caa101f69619d0ae479ad73
jkeyser <jake.keyser@colostate.edu>
2020-04-04 12:06:15 -0600

    Initial code for word count exercise. Not on the right track

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,8 @@
 def count_words(sentence):
-    pass
+    count = 0
+    for word in sentence.split(" "):
+        if word == word:
+            count +=1 
+            print(f"{word} : {count}")
+
+count_words("word and me")
