cf8de61e6618dba35c370d20ad61cf8fa03f84f7
jkeyser <jake.keyser@colostate.edu>
2020-04-13 12:44:17 -0600

    refactored to remove the first iterable

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,9 +1,7 @@
 def count_words(sentence):
     count = 0
-    word_list = []
-    for word in sentence.split():
-        word_list.append(word)
-
+    word_list = sentence.lower().split()
+    print(word_list)
     word_count = {}
 
     i = 0
@@ -14,7 +12,8 @@ def count_words(sentence):
                 count += 1
                 word_count[word] = count
         i += 1
+    print(word_count)
     return word_count
 
 
-count_words("one of each")
+count_words("one,two,three")
