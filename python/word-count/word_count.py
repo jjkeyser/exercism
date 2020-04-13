af301e035a921aac36f9e1de539f7f94229230e7
jkeyser <jake.keyser@colostate.edu>
2020-04-13 11:35:45 -0600

    code to pass first test

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,8 +1,20 @@
 def count_words(sentence):
     count = 0
-    for word in sentence.split(" "):
-        if word == word:
-            count +=1 
-            print(f"{word} : {count}")
+    word_list = []
+    for word in sentence.split():
+        word_list.append(word)
 
-count_words("word and me")
+    word_count = {}
+
+    i = 0
+    while i < len(word_list):
+        count = 0
+        for word in word_list:
+            if word == word_list[i]:
+                count += 1
+                word_count[word] = count
+        i += 1
+        return word_count
+
+
+count_words("one of each")
