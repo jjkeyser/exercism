f071b8f7d2940db2738b109d111712f11d5adb8a
jkeyser <jake.keyser@colostate.edu>
2020-04-14 10:49:23 -0600

    handles all non-exception tests

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,14 @@
 def distance(strand_a, strand_b):
-    pass
+
+    char_diff = []
+   
+    for i in range(0, len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                char_diff.append(strand_b[i])
+            i += 1
+    
+    print(len(char_diff))
+    return len(char_diff)
+
+
+distance("AATG", "AAA")
