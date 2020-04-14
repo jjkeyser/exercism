83039a635eee62c03cfb795bc2eb3e06dbd1cdc5
jkeyser <jake.keyser@colostate.edu>
2020-04-14 11:27:42 -0600

    refactored to remove the the unnecessary list

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,17 +1,13 @@
 def distance(strand_a, strand_b):
 
-    char_diff = []
-    
+    distance_count = 0
+
     if len(strand_a) != len(strand_b):
         raise ValueError("The strands must be the same length")
     else:
         for i in range(0, len(strand_a)):
-                if strand_a[i] != strand_b[i]:
-                    char_diff.append(strand_b[i])
-                i += 1
+            if strand_a[i] != strand_b[i]:
+                distance_count += 1
+            i += 1
     
-    print(len(char_diff))
-    return len(char_diff)
-
-
-distance("AATG", "AAAB")
+    return distance_count
