206949ca126cf47b9046505a7ea8690ec5e17e4b
jkeyser <jake.keyser@colostate.edu>
2020-04-14 11:12:41 -0600

    Final solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,14 +1,17 @@
 def distance(strand_a, strand_b):
 
     char_diff = []
-   
-    for i in range(0, len(strand_a)):
-            if strand_a[i] != strand_b[i]:
-                char_diff.append(strand_b[i])
-            i += 1
+    
+    if len(strand_a) != len(strand_b):
+        raise ValueError("The strands must be the same length")
+    else:
+        for i in range(0, len(strand_a)):
+                if strand_a[i] != strand_b[i]:
+                    char_diff.append(strand_b[i])
+                i += 1
     
     print(len(char_diff))
     return len(char_diff)
 
 
-distance("AATG", "AAA")
+distance("AATG", "AAAB")
