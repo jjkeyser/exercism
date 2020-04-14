4673e1fbdb9d8fb2daad3fd701c5b328914b875f
jkeyser <jake.keyser@colostate.edu>
2020-04-14 09:50:11 -0600

    final refactored solution

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -4,20 +4,11 @@ import re
 def count_words(sentence):
 
     word_list = re.findall("[a-z0-9]+'*[0-9a-z]+|[0-9a-z]+", sentence.lower())
-    print(word_list)
     word_count = {}
 
-    count = 0
-    i = 0
-    while i < len(word_list):
-        count = 0
-        for word in word_list:
-            if word == word_list[i]:
-                count += 1
-                word_count[word] = count
-        i += 1
-    print(word_count)
+    for word in word_list:
+        if word not in word_count.keys():
+            word_count[word] = 1
+        else:
+            word_count[word] += 1
     return word_count
-
-
-count_words("1: one two's")
