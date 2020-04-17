d625a5a970777ce76e9406bcf45a97a4b39b8379
jkeyser <jake.keyser@colostate.edu>
2020-04-17 11:59:25 -0600

    passes row tests

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,19 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self._matrix_string = matrix_string
 
     def row(self, index):
-        pass
+        matrix_row = self._matrix_string.splitlines()
+        matrix_row_list = []
+        for num in matrix_row[index - 1].split():
+            num = int(num)
+            matrix_row_list.append(num)
+        return matrix_row_list
 
     def column(self, index):
         pass
+
+
+x = Matrix("1 2 3\n4 5 6")
+
+print(x.row(1))
