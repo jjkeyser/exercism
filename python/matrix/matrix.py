1be6efb99d0de4002638ca618f6b60dcfc822da4
jkeyser <jake.keyser@colostate.edu>
2020-04-19 14:09:27 -0600

    matrix refactored working solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,19 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self._matrix_string = matrix_string
+        matrix_rows_string = matrix_string.splitlines()
+        self.matrix_rows = [
+            [int(num) for num in row.split()] for row in matrix_rows_string
+        ]
 
     def row(self, index):
-        matrix_row = self._matrix_string.splitlines()
-        matrix_row_list = []
-        for num in matrix_row[index - 1].split():
-            num = int(num)
-            matrix_row_list.append(num)
-        return matrix_row_list
+        return self.matrix_rows[index - 1]
 
     def column(self, index):
-        pass
-
-
-x = Matrix("1 2 3\n4 5 6")
-
-print(x.row(1))
+        return [row[index - 1] for row in self.matrix_rows]
