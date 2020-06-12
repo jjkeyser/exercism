b80991a6f848c6281307c985980a5e07a1b75b3e
jjkeyser <jakob.keyser@gmail.com>
2020-06-12 08:57:00 -0600

    initial passing solution

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,10 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) 
+        {
+            return true;
+        }
+        return false;
     }
 }
