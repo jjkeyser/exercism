89aef8df8b75385a4a4785e60eded21ae7053393
jjkeyser <jakob.keyser@gmail.com>
2020-06-15 09:37:07 -0600

    working solution but hacky, needs refactoring

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -4,11 +4,19 @@ public static class ResistorColor
 {
     public static int ColorCode(string color)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string[] colorMap = Colors();
+        for (int i = 0; i < colorMap.Length; i++)
+        {
+            if (colorMap[i] == color)
+            {
+                return i;
+            }
+        }
+        return 100;
     }
 
     public static string[] Colors()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     }
 }
