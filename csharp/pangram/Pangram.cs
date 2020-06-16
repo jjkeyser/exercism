669b13a198532dfc7e97c998e26fe846ef443a41
jjkeyser <jakob.keyser@gmail.com>
2020-06-16 10:23:08 -0600

    Initial working solution

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        string lowerInput = input.ToLower();
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+        List<string> charList = new List<string>();
+
+        foreach (var letter in alphabet)
+        {
+            if (lowerInput.Contains(letter) && charList.Contains(letter.ToString()) == false)
+            {
+                charList.Add(letter.ToString());
+            }
+        }
+
+        return (charList.Count == alphabet.Length) ? true : false;
     }
 }
