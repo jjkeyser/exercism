9d6bd2fe5cedea1f874e8bf741b1d50b021b11df
jjkeyser <jakob.keyser@gmail.com>
2020-06-17 08:40:15 -0600

    reset matrix exercise to starting code

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,12 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
-        matrix_rows_string = matrix_string.splitlines()
-        self.matrix_rows = [
-            [int(num) for num in row.split()] for row in matrix_rows_string
-        ]
+        pass
 
     def row(self, index):
-        return self.matrix_rows[index - 1]
+        pass
 
     def column(self, index):
-        return [row[index - 1] for row in self.matrix_rows]
+        pass
