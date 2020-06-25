b8b406be19de9bb2588dbaa40a0893a4a24f3426
jjkeyser <jakob.keyser@gmail.com>
2020-06-25 10:52:52 -0600

    initial working solution

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,17 @@
+import random
+import string
+
+
 class Robot:
     def __init__(self):
-        pass
+        self.name = self.set_name()
+
+    def set_name(self):
+        prefix = "".join(random.choices(string.ascii_uppercase, k=2))
+        number = random.randint(100, 999)
+
+        return prefix + str(number)
+
+    def reset(self):
+        while self.name == self.set_name():
+            self.name = self.set_name()
