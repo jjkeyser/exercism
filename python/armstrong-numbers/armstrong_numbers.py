4e35b569d5ae63a619afdde0ea5f571a68175bf9
jjkeyser <jakob.keyser@gmail.com>
2020-06-26 16:25:53 -0600

    initial working solution

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,13 @@
+import math
+
 def is_armstrong_number(number):
-    pass
+    number_str = str(number)
+    number_length = len(number_str)
+    digit_sum = 0
+    for digit in number_str:
+        digit_sum += math.pow(int(digit), number_length)
+    
+    return True if digit_sum == number else False
+
+
+is_armstrong_number(5678)
