88bd13d9eeff3d7b557c4d278d923b3702d35733
jjkeyser <jakob.keyser@gmail.com>
2020-06-26 16:33:02 -0600

    simplified return statement

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -8,4 +8,4 @@ def is_armstrong_number(number):
     for digit in number_str:
         digit_sum += math.pow(int(digit), number_length)
 
-    return True if digit_sum == number else False
+    return digit_sum == number
