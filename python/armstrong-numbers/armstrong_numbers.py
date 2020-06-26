536b9ee4afe094b05766064f74d53d8fa3430e17
jjkeyser <jakob.keyser@gmail.com>
2020-06-26 16:26:37 -0600

    removed test function call from code

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,13 +1,11 @@
 import math
 
+
 def is_armstrong_number(number):
     number_str = str(number)
     number_length = len(number_str)
     digit_sum = 0
     for digit in number_str:
         digit_sum += math.pow(int(digit), number_length)
-    
+
     return True if digit_sum == number else False
-
-
-is_armstrong_number(5678)
