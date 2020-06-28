319803c21dfc41ffa3a76b63455f494cb8c3c60c
Jake Keyser <jake.keyser@colostate.edu>
2020-06-28 10:51:36 -0600

    initial working solution

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,11 @@
 def find_anagrams(word, candidates):
-    pass
+    matches = []
+    letter_list = sorted([letter for letter in word.lower()])
+    candidate_list = []
+    for candidate in candidates:
+        candidate_list.append(sorted([letter for letter in candidate.lower()]))
+    i = 0
+    for i in range(0, len(candidate_list)):
+        if candidate_list[i] == letter_list and candidates[i].lower() != word.lower():
+            matches.append(candidates[i])
+    return(matches)
