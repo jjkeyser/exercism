b9d3f10af823f8fa7bde325a92e53a2ada8d94b4
Jake Keyser <jake.keyser@colostate.edu>
2020-06-28 13:01:57 -0600

    removed unecessary lists

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,11 +1,7 @@
 def find_anagrams(word, candidates):
-    matches = []
-    letter_list = sorted([letter for letter in word.lower()])
-    candidate_list = []
+    anagrams = []
     for candidate in candidates:
-        candidate_list.append(sorted([letter for letter in candidate.lower()]))
-    i = 0
-    for i in range(0, len(candidate_list)):
-        if candidate_list[i] == letter_list and candidates[i].lower() != word.lower():
-            matches.append(candidates[i])
-    return(matches)
+        if candidate.lower() != word.lower():
+            if sorted(candidate.lower()) == sorted(word.lower()):
+                anagrams.append(candidate)
+    return anagrams
