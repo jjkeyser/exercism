9e76c3c1c6363ba5121410570bbe8db649d9132c
Jake Keyser <jake.keyser@colostate.edu>
2020-06-29 16:19:32 -0600

    refactored to use list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,13 +1,8 @@
 def distance(strand_a, strand_b):
 
-    distance_count = 0
+    # distance_count = 0
 
     if len(strand_a) != len(strand_b):
         raise ValueError("The strands must be the same length")
     else:
-        for i in range(0, len(strand_a)):
-            if strand_a[i] != strand_b[i]:
-                distance_count += 1
-            i += 1
-    
-    return distance_count
+        return len([i for i in range(0, len(strand_a)) if strand_a[i] != strand_b[i]])
