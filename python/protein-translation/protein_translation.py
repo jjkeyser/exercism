530b80213f8fbccfcf5d9e3cc03ebbbfa91c1eae
jjkeyser <jakob.keyser@gmail.com>
2020-07-05 14:17:45 -0600

    passes three character sequence tests

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,23 @@
 def proteins(strand):
-    pass
+    proteins = {
+        "Methionine": ["AUG"],
+        "Phenylalanine": ["UUU", "UUC"],
+        "Leucine": ["UUA", "UUG"],
+        "Serine": ["UCU", "UCC", "UCA", "UCG"],
+        "Tyrosine": ["UAU", "UAC"],
+        "Cysteine": ["UGU", "UGC"],
+        "Tryptophan": ["UGG"],
+        "STOP": ["UAA", "UAG", "UGA"],
+    }
+    protein_list = []
+    for key, values in proteins.items():
+        for value in values:
+            if strand == 'UAA' or strand == 'UAG' or strand == 'UGA':
+                continue
+            elif strand == value:
+                protein_list.append(key)
+    # print(protein_list)
+    return protein_list
+
+
+proteins("UAA")
