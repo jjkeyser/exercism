28f41102cc25a937f03df351df0c243814819619
jjkeyser <jakob.keyser@gmail.com>
2020-07-06 15:49:54 -0600

    refactored to use list comprehension and sum()

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -2,10 +2,6 @@ import math
 
 
 def is_armstrong_number(number):
-    number_str = str(number)
-    number_length = len(number_str)
-    digit_sum = 0
-    for digit in number_str:
-        digit_sum += math.pow(int(digit), number_length)
-
-    return digit_sum == number
+    return number == sum(
+        [math.pow(int(digit), len(str(number))) for digit in str(number)]
+    )
