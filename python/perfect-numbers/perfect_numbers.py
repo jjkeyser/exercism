7f8e345436bb5bb352f6205413c73747cada8360
jjkeyser <jakob.keyser@gmail.com>
2020-07-06 13:01:27 -0600

    used list comprehension to get the factors instead of while loop

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,16 +1,13 @@
 def classify(number):
-    aliquot_sum = 0
-    i = 1
+
     if number <= 0:
         raise ValueError("The number cannot be 0 or negative!")
 
-    while i < number:
-        if number % i == 0:
-            aliquot_sum += i
-        i += 1
-    if aliquot_sum == number:
+    factors = [i for i in range(1, number - 1) if number % i == 0]
+
+    if sum(factors) == number:
         return "perfect"
-    elif aliquot_sum > number:
+    elif sum(factors) > number:
         return "abundant"
     else:
         return "deficient"
