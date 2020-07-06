610bb3866507e7b31bf8b7d009c2f13669e842ca
jjkeyser <jakob.keyser@gmail.com>
2020-07-06 12:16:16 -0600

    initial working solution

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,16 @@
 def classify(number):
-    pass
+    aliquot_sum = 0
+    i = 1
+    if number == 0 or number < 0:
+        raise ValueError("The number cannot be 0 or negative!")
+
+    while i < number:
+        if number % i == 0:
+            aliquot_sum += i
+        i += 1
+    if aliquot_sum == number:
+        return "perfect"
+    elif aliquot_sum > number:
+        return "abundant"
+    else:
+        return "deficient"
