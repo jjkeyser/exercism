52f550212b53d9f58d7428a5c4588b674d6f8c50
jjkeyser <jakob.keyser@gmail.com>
2020-07-06 17:10:37 -0600

    used hashset and IsSubsetOf() for solution

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    static HashSet<char> alphabet = new HashSet<char>("abcdefghijklmnopqrstuvwxyz");
+
+    public static bool IsPangram(string input)
+    {
+        return alphabet.IsSubsetOf(input.ToLower());
+    }
+}
