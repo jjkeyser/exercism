6aa212c4ec9cd12492040e67decc3c6dd9447e49
jjkeyser <jakob.keyser@gmail.com>
2020-07-07 11:35:44 -0600

    ran black linter to fix formatting

--- python/series/series.py
+++ python/series/series.py
@@ -1,9 +1,9 @@
 def slices(series, length):
     if length > len(series):
         raise ValueError("The slice length can't exceed the series")
-    elif length <=0:
+    elif length <= 0:
         raise ValueError("The slice length must be greater than 0")
     elif series is "":
         raise ValueError("The series cannot be empty")
-    
-    return [series[i : length + i] for i in range(0, len(series)-length+1)]
+
+    return [series[i : length + i] for i in range(0, len(series) - length + 1)]
