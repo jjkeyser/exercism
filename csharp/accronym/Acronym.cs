0b82df20f75642d932bfde7433bd76925bf96ab2
jjkeyser <jakob.keyser@gmail.com>
2020-07-08 16:36:54 -0600

    initial working solution

--- csharp/accronym/Acronym.cs
+++ csharp/accronym/Acronym.cs
@@ -4,6 +4,15 @@ public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        char[] splitChars = {' ', '-', '_'};
+        string[] phrase_list = phrase.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+        string acronym = "";
+        
+        foreach (string word in phrase_list)
+        {
+            acronym += word.Substring(0,1);
+        }
+        
+        return acronym.ToUpper();
     }
 }
