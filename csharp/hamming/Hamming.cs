270937b3988389b3a6473d94dd91b549f026fa4e
jjkeyser <jakob.keyser@gmail.com>
2020-07-09 16:30:11 -0600

    initial working solution

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,6 +4,20 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int hamming = 0;
+        
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException("The two strings must be the same length");
+        }
+        
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i])
+            {
+                hamming ++;
+            }
+        }
+        return hamming;
     }
 }
