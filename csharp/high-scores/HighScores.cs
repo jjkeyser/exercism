76985af378169dd0eab956616d6027254b4c7ef8
jjkeyser <jakob.keyser@gmail.com>
2020-07-09 14:33:28 -0600

    formatting changes

--- csharp/high-scores/HighScores.cs
+++ csharp/high-scores/HighScores.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+
 public class HighScores
 {
     private List<int> _scores;
     public HighScores(List<int> list) => _scores = new List<int>(list);
     public List<int> Scores() => _scores;
-
     public int Latest() => _scores[_scores.Count - 1];
 
     public int PersonalBest()
