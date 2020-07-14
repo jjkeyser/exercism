6fac953d710a6b3722c915ffd4343f6d48990d91
jjkeyser <jakob.keyser@gmail.com>
2020-07-14 16:51:55 -0600

    passing all tests

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,6 +5,24 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        IDictionary<char, int> Nucleotides = new Dictionary<char, int>()
+        {
+            ['A'] = 0,
+            ['C'] = 0,
+            ['G'] = 0,
+            ['T'] = 0
+        };
+
+        foreach (var letter in sequence.ToUpper())
+        {
+            if (Nucleotides.ContainsKey(letter))
+            {
+                Nucleotides[letter] += 1;
+            } else
+            {
+                throw new ArgumentException("Letter not a valid Nucleotide.");
+            }
+        }
+        return Nucleotides;
     }
 }
