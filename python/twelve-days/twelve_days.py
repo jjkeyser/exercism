e7af40cbc2a51d49ad41f2a9e026b04cf25859fa
jjkeyser <jakob.keyser@gmail.com>
2020-07-14 12:53:48 -0600

    first test passing

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,2 +1,32 @@
 def recite(start_verse, end_verse):
-    pass
+    day = [
+        "first", 
+        "second", 
+        "third", 
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eigth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"]  
+    opening_phrase = f"On the {day[end_verse - 1]} day of Christmas my true love gave to me: "
+    filler = "and "
+    lyrics = [
+        "a Partridge in a Pear Tree.", 
+        "two Turtle Doves, ", 
+        "three French Hens, ",
+        "four Calling Birds, ", 
+        "five Gold Rings, ", 
+        "six Geese-a-Laying, ",
+        "seven Swans-a-Swimming, ",
+        "eight Maids-a-Milking, ",
+        "nine Ladies Dancing, ",
+        "ten Lords-a-Leaping, ",
+        "eleven Pipers Piping, ",
+        "twelve Drummers Drumming, "]
+
+    if start_verse <= 1:
+        return [opening_phrase + str(lyrics[end_verse - 1])]
--- python/twelve-days/twelve_days_test.py
+++ python/twelve-days/twelve_days_test.py
@@ -12,7 +12,7 @@ class TwelveDaysTest(unittest.TestCase):
             "a Partridge in a Pear Tree."
         ]
         self.assertEqual(recite(1, 1), expected)
-
+    
     def test_second_day_two_turtle_doves(self):
         expected = [
             "On the second day of Christmas my true love gave to me: "
@@ -21,6 +21,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(2, 2), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_third_day_three_french_hens(self):
         expected = [
             "On the third day of Christmas my true love gave to me: "
@@ -30,6 +31,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(3, 3), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_fourth_day_four_calling_birds(self):
         expected = [
             "On the fourth day of Christmas my true love gave to me: "
@@ -40,6 +42,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(4, 4), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_fifth_day_five_gold_rings(self):
         expected = [
             "On the fifth day of Christmas my true love gave to me: "
@@ -51,6 +54,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(5, 5), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_sixth_day_six_geese_a_laying(self):
         expected = [
             "On the sixth day of Christmas my true love gave to me: "
@@ -63,6 +67,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(6, 6), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_seventh_day_seven_swans_a_swimming(self):
         expected = [
             "On the seventh day of Christmas my true love gave to me: "
@@ -76,6 +81,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(7, 7), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_eighth_day_eight_maids_a_milking(self):
         expected = [
             "On the eighth day of Christmas my true love gave to me: "
@@ -90,6 +96,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(8, 8), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_ninth_day_nine_ladies_dancing(self):
         expected = [
             "On the ninth day of Christmas my true love gave to me: "
@@ -105,6 +112,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(9, 9), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_tenth_day_ten_lords_a_leaping(self):
         expected = [
             "On the tenth day of Christmas my true love gave to me: "
@@ -121,6 +129,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(10, 10), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_eleventh_day_eleven_pipers_piping(self):
         expected = [
             "On the eleventh day of Christmas my true love gave to me: "
@@ -138,6 +147,7 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(11, 11), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_twelfth_day_twelve_drummers_drumming(self):
         expected = [
             "On the twelfth day of Christmas my true love gave to me: "
@@ -156,14 +166,17 @@ class TwelveDaysTest(unittest.TestCase):
         ]
         self.assertEqual(recite(12, 12), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_recites_first_three_verses_of_the_song(self):
         expected = [recite(n, n)[0] for n in range(1, 4)]
         self.assertEqual(recite(1, 3), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_recites_three_verses_from_the_middle_of_the_song(self):
         expected = [recite(n, n)[0] for n in range(4, 7)]
         self.assertEqual(recite(4, 6), expected)
 
+    @unittest.skip("Remove when ready to test")
     def test_recites_the_whole_song(self):
         expected = [recite(n, n)[0] for n in range(1, 13)]
         self.assertEqual(recite(1, 12), expected)
