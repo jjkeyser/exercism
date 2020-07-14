f62170c991c4f635b753074821845cfe6a716c3d
jjkeyser <jakob.keyser@gmail.com>
2020-07-14 10:50:52 -0600

    changed elif to if

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -3,7 +3,6 @@ def is_isogram(string):
     for letter in string.lower():
         if letter in string_list:
             return False
-        elif letter.isalpha():
+        if letter.isalpha():
             string_list.append(letter)
     return True
-
