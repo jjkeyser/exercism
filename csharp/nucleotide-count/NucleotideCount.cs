927d9ae9bca687a8faf3198115271d51cd5f4032
jjkeyser <jakob.keyser@gmail.com>
2020-07-14 17:01:58 -0600

    formatting

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -5,23 +5,21 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        IDictionary<char, int> count = new Dictionary<char, int>()
-        {
-            {'A', 0},
-            {'C', 0},
-            {'G', 0},
-            {'T', 0},            
-        };
+        IDictionary<char, int> count = 
+            new Dictionary<char, int>()
+            {
+                {'A', 0},
+                {'C', 0},
+                {'G', 0},
+                {'T', 0},            
+            };
 
         foreach (var letter in sequence.ToUpper())
         {
             if (count.ContainsKey(letter))
-            {
                 count[letter]++;
-            } else
-            {
+            else
                 throw new ArgumentException("Letter not a valid Nucleotide.");
-            }
         }
         return count;
     }
