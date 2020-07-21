1bf893039c1635f3880d893a5f86195dfee766d6
jjkeyser <jakob.keyser@gmail.com>
2020-07-21 13:40:19 -0600

    Passing all tests except unique

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 public class Robot
 {
+    private Random _rand = new Random();
+    private string _name;
+    private HashSet<string> _usedNames = new HashSet<string>();
+
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return _name;
         }
     }
 
+    public Robot() => _name = GetName();
+    
+    private string GenerateNameChars()
+    {
+        var randChars = "";
+        for (int i = 0; i < 2; i++)
+        {
+            randChars += (char)_rand.Next('A', 'Z');
+        }
+
+        return randChars;
+    }
+
+    private string GenerateNameDigits() => _rand.Next(100,1000).ToString();
+
+    public string GetName() => GenerateNameChars() + GenerateNameDigits();
+
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_usedNames.Contains(_name))
+        {
+            _usedNames.Remove(_name);
+        }
+        _name = GetName();
     }
 }
