165a49b8905c9f32090afca4e5b582c214c065f7
jjkeyser <jakob.keyser@gmail.com>
2020-08-03 19:53:13 -0600

    passing all tests

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        HashSet<int> countedMultiples = new HashSet<int>();
+        foreach (int multiple in multiples)
+        {
+            for (int i = multiple; i < max; i++)
+            {
+                if (multiple == 0)
+                {
+                   countedMultiples.Add(0);
+                }else if (i % multiple == 0)
+                {
+                    countedMultiples.Add(i);
+                }
+            }
+        }
+        return countedMultiples.Sum();
     }
 }
