6d342f6cd820a90284ca895b10b497604f28f45e
jjkeyser <jakob.keyser@gmail.com>
2020-08-03 18:36:37 -0600

    changed if else to use ?: operator

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -7,20 +7,14 @@ public static class CollatzConjecture
         int n = number;
         int steps = 0;
 
-        if (n <= 0)
+        if (n < 1)
         {
             throw new ArgumentOutOfRangeException("the number must be greater than zero");
         }
 
         while (n != 1)
         {
-            if (n % 2 == 0)
-            {
-                n = n / 2;
-            } else
-            {
-                n = (3 * n ) + 1;
-            }
+            n = (n % 2 == 0) ? n / 2 : 3 * n  + 1;
             steps++;
         }
         return steps;
