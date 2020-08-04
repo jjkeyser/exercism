35fd65380ea7f314b36f79aaa31b128b3e61d4cc
jjkeyser <jakob.keyser@gmail.com>
2020-08-03 18:26:49 -0600

    passing all tests

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,25 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        int n = number;
+        int steps = 0;
+
+        if (n <= 0)
+        {
+            throw new ArgumentOutOfRangeException("the number must be greater than zero");
+        }
+
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            } else
+            {
+                n = (3 * n ) + 1;
+            }
+            steps++;
+        }
+        return steps;
     }
 }
