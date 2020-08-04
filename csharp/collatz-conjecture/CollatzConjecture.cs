b38aa3b714cf72328d78b4f00df4974bc0f584b9
jjkeyser <jakob.keyser@gmail.com>
2020-08-03 18:44:54 -0600

    changed while operator to greater than 1

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -11,7 +11,7 @@ public static class CollatzConjecture
             throw new ArgumentOutOfRangeException("the number must be greater than zero");
         }
 
-        while (number != 1)
+        while (number > 1)
         {
             number = (number % 2 == 0) ? number / 2 : 3 * number  + 1;
             steps++;
