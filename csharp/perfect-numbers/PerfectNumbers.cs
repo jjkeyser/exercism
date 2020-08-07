cb80ccc7c4a302924f062ed52354c8fa8de4454e
jjkeyser <jakob.keyser@gmail.com>
2020-08-07 14:59:59 -0600

    passing all tests

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 public enum Classification
 {
     Perfect,
@@ -11,6 +12,34 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException();
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException("not a natural number");
+        }
+        
+        if(GetFactors(number).Sum() == number)
+        {
+            return Classification.Perfect;
+        } else if (GetFactors(number).Sum() > number)
+        {
+            return Classification.Abundant;
+        } else
+        {
+            return Classification.Deficient;
+        }
+    }
+
+    private static IEnumerable<int> GetFactors(int number)
+    {
+        var factors = new List<int>();
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                factors.Add(i);
+            }
+        }
+        return factors;
     }
 }
