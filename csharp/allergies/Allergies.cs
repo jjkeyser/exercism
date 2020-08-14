e6aebfc944b4f7241550b8b18a16f723811eb38e
jjkeyser <jakob.keyser@gmail.com>
2020-08-14 15:57:31 -0600

    passing all tests

--- csharp/allergies/Allergies.cs
+++ csharp/allergies/Allergies.cs
@@ -1,30 +1,36 @@
 using System;
+using System.Linq;
 
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
+    private int _score {get; set;}
     public Allergies(int mask)
     {
+        _score = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return (_score & (int)allergen) > 0;
     }
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enum.GetValues(typeof(Allergen))
+                    .Cast<Allergen>()
+                    .Where(a => IsAllergicTo(a))
+                    .ToArray();
     }
 }
