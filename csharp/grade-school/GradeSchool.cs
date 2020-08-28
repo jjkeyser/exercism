49a9b4bdd2fbd3580daef347840da2bb544d7dfc
jjkeyser <jakob.keyser@gmail.com>
2020-08-28 16:41:54 -0600

    passing first 5 tests

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private Dictionary<int, List<string>> _StudentRoster = new Dictionary<int, List<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!_StudentRoster.ContainsKey(grade))
+        {
+            _StudentRoster.Add(grade, new List<string>{student});
+        }
+        else
+        {
+            _StudentRoster[grade].Add(student);
+            _StudentRoster[grade].Sort();
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<string> students = new List<string>();
+        List<int> grades = new List<int> (_StudentRoster.Keys);
+        foreach (int grade in grades.OrderBy(g => g))
+        {
+            foreach (string student in _StudentRoster[grade])
+            {
+                students.Add(student);
+            }
+        }
+        return students;
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        throw new NotImplementedException();
     }
 }
