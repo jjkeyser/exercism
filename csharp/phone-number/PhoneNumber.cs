ea60df4a2192648d48e1bedb58e92e039519f10b
jjkeyser <jakob.keyser@gmail.com>
2020-09-04 14:09:57 -0600

    refactored to combine redundant code

--- csharp/phone-number/PhoneNumber.cs
+++ csharp/phone-number/PhoneNumber.cs
@@ -7,42 +7,16 @@ public class PhoneNumber
     {
         string number = Regex.Replace(phoneNumber, "[^0-9]", "");
 
-        if (number.Length > 11 || number.Length < 10)
+        if (number.Length > 11 
+            || number.Length < 10
+            || number[number.Length - 10] < '2'
+            || number[number.Length - 7] < '2'
+            || (number.Length == 11 && number[0] != '1')
+           )
         {
             throw new ArgumentException("Invalid phone number length");
         }
 
-        if (number.Length == 11)
-        {
-            if (!CountryCodeIsOne(number))
-            {
-                throw new ArgumentException("The country code must be 1");
-            }
-            number = number.Substring(1);
-        }
-        
-        if (!AreaCodeIsValid(number) || !ExchangeCodeIsValid(number))
-        {
-            throw new ArgumentException("Invalid phone number.");
-        }
-
-        return number;
-    }
-
-    public static bool CountryCodeIsOne(string cleanNumber)
-    {
-        return char.GetNumericValue(cleanNumber[0]) == 1;
-    }
-
-    public static bool AreaCodeIsValid(string cleanNumber)
-    {
-        var number = (int)char.GetNumericValue(cleanNumber[0]);
-        return number > 1 && number < 10;
-    }
-
-    public static bool ExchangeCodeIsValid(string cleanNumber)
-    {
-        var number = (int)char.GetNumericValue(cleanNumber[3]);
-        return number > 1 && number < 10;
+        return number.Length == 11 ? number.Substring(1) : number;
     }
 }
