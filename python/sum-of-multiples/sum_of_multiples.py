952f15b300d51a16320fa4e98de201bbfc87839a
jjkeyser <jakob.keyser@gmail.com>
2020-09-07 15:40:52 -0600

    passing all tests

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,7 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit, divisors):
+    multiples = set()
+    for num in range(1, limit):
+        for d in divisors:
+            if d != 0 and num % d == 0:
+                multiples.add(num)
+    return sum(multiples)
