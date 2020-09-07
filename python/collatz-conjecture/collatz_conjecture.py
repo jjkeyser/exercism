70f26430bbf6d2ab7259aec83e28c8c0074d627c
jjkeyser <jakob.keyser@gmail.com>
2020-09-07 16:12:34 -0600

    passing all tests

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError("Invalid number")
+
+    collatz_number = number
+    count = 0
+
+    while collatz_number != 1:
+        if collatz_number % 2 == 0:
+            collatz_number = collatz_number / 2
+        else:
+            collatz_number = 3 * collatz_number + 1
+        count += 1
+
+    return count
