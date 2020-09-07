89fc68347afcbdb4101d70e5e8a1d22465f3f763
jjkeyser <jakob.keyser@gmail.com>
2020-09-07 16:14:50 -0600

    removed redundant variable

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -2,14 +2,13 @@ def steps(number):
     if number < 1:
         raise ValueError("Invalid number")
 
-    collatz_number = number
     count = 0
 
-    while collatz_number != 1:
-        if collatz_number % 2 == 0:
-            collatz_number = collatz_number / 2
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
         else:
-            collatz_number = 3 * collatz_number + 1
+            number = 3 * number + 1
         count += 1
 
     return count
