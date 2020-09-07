c40c041234eaad6f565b12d3fef43d0183d3a2e2
jjkeyser <jakob.keyser@gmail.com>
2020-09-07 16:26:10 -0600

    changed counter variable name to steps to reflect the purpose better

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -2,10 +2,10 @@ def steps(number):
     if number < 1:
         raise ValueError("Invalid number")
 
-    count = 0
+    steps = 0
 
     while number != 1:
         number = 3*number+1 if number%2 else number//2
-        count += 1
+        steps += 1
 
-    return count
+    return steps
