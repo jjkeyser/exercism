74848b5cde54f64e49c70ef2c9b19eebb93d6345
jjkeyser <jakob.keyser@gmail.com>
2020-09-09 11:48:37 -0600

    added check for irregular matrix

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,5 @@
 def saddle_points(matrix):
-    pass
+    for i in range(0, len(matrix)):
+        row_len = len(matrix[0])
+        if len(matrix[i]) != row_len:
+            raise ValueError("Irregular matrix!")
