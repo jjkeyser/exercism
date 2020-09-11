69bcb9877846b40eb21ec8aa79d7571a62f1c832
jjkeyser <jakob.keyser@gmail.com>
2020-09-11 12:19:24 -0600

    passing all tests

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,33 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 public static class ScrabbleScore
 {
+    private static IDictionary<int, char[]> SCORES = new Dictionary<int, char[]>()
+    {
+        {1, new char[] {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'}},
+        {2, new char[]{'D', 'G'}},
+        {3, new char[]{'B', 'C', 'M', 'P'}},
+        {4, new char[]{'F', 'H', 'V', 'W', 'Y'}},
+        {5, new char[]{'K'}},
+        {8, new char[]{'J', 'X'}},
+        {10, new char[]{'Q', 'Z'}}
+    };
+    
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] letters = input.ToUpper().ToCharArray();
+        int score = 0;
+        foreach (var letter in letters)
+        {
+            foreach (var key in SCORES.Keys)
+            {
+                if (SCORES[key].Contains(letter))
+                {
+                    score += key;
+                }
+            }
+        }
+        return score;
     }
 }
