c20029437a7a8bbf2fa75251df5eb945d8875125
jjkeyser <jakob.keyser@gmail.com>
2020-09-15 16:51:23 -0600

    passing all tests

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,5 +1,24 @@
 def saddle_points(matrix):
+    columns = list(zip(*matrix))
+    saddle_points = []
+
+    if len(matrix) == 0:
+        return saddle_points
+
+    if is_irregular_matrix(matrix):
+        raise ValueError("Irregular matrix!")
+
+    for i, row in enumerate(matrix):
+        for j, num in enumerate(row):
+            if num == max(row) and num == min(columns[j]):
+                saddle_points.append({"row": i + 1, "column": j + 1})
+        
+
+    return saddle_points
+
+
+def is_irregular_matrix(matrix):
+    row_len = len(matrix[0])
     for i in range(0, len(matrix)):
-        row_len = len(matrix[0])
-        if len(matrix[i]) != row_len:
-            raise ValueError("Irregular matrix!")
+        if (len(matrix[i]) != row_len):
+            return True
