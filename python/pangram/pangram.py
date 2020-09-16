7db322b38e36473b0e29aa4c362f0aa6dc682fda
jjkeyser <jakob.keyser@gmail.com>
2020-09-16 14:09:44 -0600

    removed commented out code

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,9 +4,4 @@ def is_pangram(sentence):
     for char in sentence.lower():
         if char not in letters and char in alphabet:
             letters.append(char)
-    # i = 0
-    # while i < len(sentence):
-    #     if sentence[i].lower() not in letters and sentence[i].lower() in alphabet:
-    #         letters.append(sentence[i].lower())
-    #     i += 1
     return len(letters) == len(alphabet)
