6e92eaa21f726da53f5f1870049a5bef7be1ded6
jjkeyser <jakob.keyser@gmail.com>
2020-09-16 14:07:27 -0600

    refactored to use for instead of while and simplified return statement

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,9 +1,12 @@
 def is_pangram(sentence):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     letters = []
-    i = 0
-    while i < len(sentence):
-        if sentence[i].lower() not in letters and sentence[i].lower() in alphabet:
-            letters.append(sentence[i].lower())
-        i += 1
-    return True if len(letters) == len(alphabet) else False
+    for char in sentence.lower():
+        if char not in letters and char in alphabet:
+            letters.append(char)
+    # i = 0
+    # while i < len(sentence):
+    #     if sentence[i].lower() not in letters and sentence[i].lower() in alphabet:
+    #         letters.append(sentence[i].lower())
+    #     i += 1
+    return len(letters) == len(alphabet)
