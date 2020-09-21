303267763d88b2ef9c1d980e530fbf6563b513d0
jjkeyser <jakob.keyser@gmail.com>
2020-09-21 16:23:09 -0600

    refactored to use sets to determine the type of triangle and consolidated triangle validation statement

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,35 +1,20 @@
 def equilateral(sides):
-    if is_triangle(sides):
-        return sides[0] == min(sides) and sides[0] == max(sides)
-    return False
+    return is_triangle(sides) and len(set(sides)) == 1
 
 
 def isosceles(sides):
-    if is_triangle(sides):
-        return (
-            sides[0] == sides[1] and sides[0] != sides[2]
-            or sides[1] == sides[2] and sides[1] != sides[0]
-            or sides[0] == sides[2] and sides[0] != sides[1]
-            or equilateral(sides)
-        )
-    return False
+    return is_triangle(sides) and len(set(sides)) <= 2
+
 
 def scalene(sides):
-    if is_triangle(sides):
-        return (
-            sides[0] != sides[1] and sides[0] != sides[2]
-        )
-    return False
+    return is_triangle(sides) and len(set(sides)) == 3
 
 
 def is_triangle(sides):
-    if len(sides) != 3:
-        return False
-    for side in sides:
-        if side <= 0:
-            return False
     if (
-        sides[0] + sides[1] < sides[2]
+        len(sides) != 3
+        or 0 in sides
+        or sides[0] + sides[1] < sides[2]
         or sides[1] + sides[2] < sides[0]
         or sides[2] + sides[0] < sides[1]
     ):
