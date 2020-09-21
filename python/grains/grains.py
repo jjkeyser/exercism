9cf33eaf11324669773d750356a06df4d1d5bbe0
Jake Keyser <jake.keyser@colostate.edu>
2020-09-21 08:26:55 -0600

    grains and triangle passing all tests

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError("Number must be <= 64 and >= 1")
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return sum([square(i) for i in range(1, 65)])
--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,35 @@
 def equilateral(sides):
-    pass
+    if is_triangle(sides):
+        return sides[0] == min(sides) and sides[0] == max(sides)
+    return False
 
 
 def isosceles(sides):
-    pass
-
+    if is_triangle(sides):
+        return (
+            sides[0] == sides[1] and sides[0] != sides[2]
+            or sides[1] == sides[2] and sides[1] != sides[0]
+            or sides[0] == sides[2] and sides[0] != sides[1]
+            or equilateral(sides)
+        )
+    return False
 
 def scalene(sides):
-    pass
+    if is_triangle(sides):
+        return (
+            sides[0] != sides[1] and sides[0] != sides[2]
+        )
+    return False
+
+
+def is_triangle(sides):
+    for side in sides:
+        if side <= 0:
+            return False
+    if (
+        sides[0] + sides[1] < sides[2]
+        or sides[1] + sides[2] < sides[0]
+        or sides[2] + sides[0] < sides[1]
+    ):
+        return False
+    return True
