d2e2e664d8c979f3555668c81d41d024377a316d
jjkeyser <jakob.keyser@gmail.com>
2020-09-22 14:00:28 -0600

    passing all tests

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,12 @@
 def to_rna(dna_strand):
-    pass
+    rna = ""
+    for dna in dna_strand.upper():
+        if dna == 'G':
+            rna += 'C'
+        if dna == 'C':
+            rna += 'G'
+        if dna == 'T':
+            rna += 'A'
+        if dna == 'A':
+            rna += 'U'
+    return rna
