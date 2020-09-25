43f64308d22125a48041e3905fca163e085cbe28
jjkeyser <jakob.keyser@gmail.com>
2020-09-25 16:21:09 -0600

    passing all tests

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,53 @@
 using System;
-
+using System.Linq;
 public static class LargestSeriesProduct
 {
     public static long GetLargestProduct(string digits, int span) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var largestProduct = 1;
+
+        foreach (char d in digits)
+        {
+            if (!char.IsDigit(d))
+            {
+                throw new ArgumentException("String must only contain digits");
+            }
+        }
+
+        if (span > digits.Length || span < 0){
+            throw new ArgumentException("Invalid Span");
+        }
+
+        if (digits == "" || span == 0)
+        {
+            return 1;
+        }
+
+        if (span == digits.Length)
+        {
+            foreach (var d in digits)
+            {
+                largestProduct *= (int)char.GetNumericValue(d);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < (digits.Length + 1) - span; i++)
+            {
+                var currentProduct = 1;
+                var currentSubstring = digits.Substring(i,span);
+                
+                foreach (var d in currentSubstring)
+                {
+                    currentProduct *= (int)char.GetNumericValue(d);
+                }
+
+                if (currentProduct > largestProduct)
+                {
+                    largestProduct = currentProduct;
+                }
+            }
+        }
+        return largestProduct == 1 ? 0 : largestProduct;
     }
 }
