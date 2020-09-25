fb3af6aef8bbb6f503ddc37b427b4f132d39d445
jjkeyser <jakob.keyser@gmail.com>
2020-09-25 10:35:39 -0600

    passing all tests

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -2,28 +2,61 @@
 
 public class CircularBuffer<T>
 {
+    private readonly T[] _buffer;
+    private readonly int _capacity;
+    private int _StartPointer = 0;
+    private int _EndPointer = 0;
+    private int _CountInBuffer = 0;
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _buffer = new T[capacity];
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_CountInBuffer == 0)
+        {
+            throw new InvalidOperationException("Can't read an empty buffer");
+        }
+        T value = _buffer[_StartPointer];
+        _StartPointer = (_StartPointer + 1) % _buffer.Length;
+        _CountInBuffer --;
+
+        return value;
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_CountInBuffer == _buffer.Length)
+        {
+            throw new InvalidOperationException("Can't write to a full buffer.");
+        }
+        _buffer[_EndPointer] = value;
+        _EndPointer = (_EndPointer + 1) % _buffer.Length;
+        _CountInBuffer ++;
     }
 
     public void Overwrite(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_CountInBuffer != _buffer.Length)
+        {
+            Write(value);
+        }
+        else
+        {
+            _CountInBuffer --;
+            Write(value);
+            _StartPointer = (_StartPointer + 1) % _buffer.Length;
+        }
     }
 
     public void Clear()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_CountInBuffer == 0)
+        {
+            return;
+        }
+        Array.Clear(_buffer, _StartPointer, 1);
+        _CountInBuffer --;
     }
 }
