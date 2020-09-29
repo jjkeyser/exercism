3b9d35bee2a6582fc0aaff3b501b7c992d93264b
jjkeyser <jakob.keyser@gmail.com>
2020-09-29 07:42:12 -0600

    passing all tests

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+  for(var i = 0; i < COLORS.length; i++){
+    if(COLORS[i] === color){
+      return i;
+    }
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
