6b496e745cba6e285c72c2cc7e871ae26891d26e
jjkeyser <jakob.keyser@gmail.com>
2020-09-30 06:24:31 -0600

    changed private class variables to start with _lowercase, and extracted an increment method for the pointer index increments

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -3,9 +3,9 @@
 public class CircularBuffer<T>
 {
     private readonly T[] _buffer;
-    private int _StartPointer = 0;
-    private int _EndPointer = 0;
-    private int _CountInBuffer = 0;
+    private int _startPointer = 0;
+    private int _endPointer = 0;
+    private int _countInBuffer = 0;
     
     public CircularBuffer(int capacity)
     {
@@ -14,44 +14,46 @@ public class CircularBuffer<T>
 
     public T Read()
     {
-        if (_CountInBuffer == 0)
+        if (_countInBuffer == 0)
         {
             throw new InvalidOperationException("Can't read an empty buffer");
         }
-        T value = _buffer[_StartPointer];
-        _StartPointer = (_StartPointer + 1) % _buffer.Length;
-        _CountInBuffer --;
+        T value = _buffer[_startPointer];
+        _startPointer = NextIndex(_startPointer);
+        _countInBuffer --;
 
         return value;
     }
 
     public void Write(T value)
     {
-        if (_CountInBuffer == _buffer.Length)
+        if (_countInBuffer == _buffer.Length)
         {
             throw new InvalidOperationException("Can't write to a full buffer.");
         }
-        _buffer[_EndPointer] = value;
-        _EndPointer = (_EndPointer + 1) % _buffer.Length;
-        _CountInBuffer ++;
+        _buffer[_endPointer] = value;
+        _endPointer = NextIndex(_endPointer);
+        _countInBuffer ++;
     }
 
     public void Overwrite(T value)
     {
-        if (_CountInBuffer == _buffer.Length)
+        if (_countInBuffer == _buffer.Length)
         {
-            _CountInBuffer --;
-            _StartPointer = (_StartPointer + 1) % _buffer.Length;
+            _countInBuffer --;
+            _startPointer = NextIndex(_startPointer);
         }
         Write(value);
     }
 
     public void Clear()
     {
-        if (_CountInBuffer > 0)
+        if (_countInBuffer > 0)
         {
-            Array.Clear(_buffer, _StartPointer, 1);
-            _CountInBuffer --;
+            Array.Clear(_buffer, _startPointer, 1);
+            _countInBuffer --;
         }
     }
+
+    private int NextIndex(int index) => ++index % _buffer.Length;
 }
