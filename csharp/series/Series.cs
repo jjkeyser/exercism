110d06844c6af7a7393e1b680c2b8457881ac316
jjkeyser <jakob.keyser@gmail.com>
2020-10-13 10:25:08 -0600

    passing all tests

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<string> series = new List<string>();
+
+        if (sliceLength > numbers.Length || sliceLength < 1 || numbers == "") {
+            throw new ArgumentException();
+        }
+
+        for (int i = 0; i < numbers.Length - sliceLength + 1; i++) {
+            series.Add(numbers.Substring(i, sliceLength));
+        }
+
+        return series.ToArray();
     }
 }
