d3244ff3d5bf297f531349c1eba40cc1ed9cfc41
jjkeyser <jakob.keyser@gmail.com>
2020-10-14 08:23:24 -0600

    passing all tests

--- csharp/prime-factors/PrimeFactors.cs
+++ csharp/prime-factors/PrimeFactors.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
     public static long[] Factors(long number)
     {
-        throw new NotImplementedException();
+        List<long> primeFactors = new List<long>();
+
+        for (int a = 2; number > 1; a++) 
+        {
+            if (number % a == 0) 
+            {
+                while (number % a == 0) 
+                {
+                    primeFactors.Add(a);
+                    number /= a;
+                }
+            }
+        }
+
+        return primeFactors.ToArray();
     }
 }
