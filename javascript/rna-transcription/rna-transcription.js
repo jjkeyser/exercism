d5b66577fe0072ce898b0abff33894a92001faa6
jjkeyser <jakob.keyser@gmail.com>
2020-10-20 13:41:49 -0600

    declared i in loop instead

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,9 +2,8 @@
 export const toRna = (dna) => {
   let rna = '';
   let dnaMap = {G:'C', C:'G', T:'A', A:'U'};
-  let i;
 
-  for (i = 0; i < dna.length; i++) {
+  for (let i = 0; i < dna.length; i++) {
       rna += dnaMap[dna[i]];
   }
 
