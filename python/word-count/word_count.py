34528e7b89e2e55e98b681d60f6e82faa9ca41e3
jjkeyser <jakob.keyser@gmail.com>
2020-10-21 16:34:48 -0600

    removed loop entirely and returned the count directly on the word list created from the regex findall

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -5,10 +5,4 @@ from collections import Counter
 def count_words(sentence):
 
     pattern = "[a-z0-9]+'*[0-9a-z]+|[0-9a-z]+"
-    word_list = re.findall(pattern, sentence.lower())
-    word_count = Counter()
-
-    for word in word_list:
-        word_count[word] += 1
-
-    return word_count
+    return Counter(re.findall(pattern, sentence.lower()))
