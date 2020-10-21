e465eec849758a3562c6459a80b90388d5ebad93
jjkeyser <jakob.keyser@gmail.com>
2020-10-20 21:01:33 -0600

    passing all tests

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,8 +1,44 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const translate = () => {
-  throw new Error("Remove this statement and implement this function");
+export const translate = (rnaSequence = '') => {
+  let codonMap = {
+    AUG:'Methionine',
+    UUU:'Phenylalanine',
+    UUC:'Phenylalanine',
+    UUA:'Leucine',
+    UUG:'Leucine',
+    UCU:'Serine',
+    UCC:'Serine',
+    UCA:'Serine',
+    UCG:'Serine',
+    UAU:'Tyrosine',
+    UAC:'Tyrosine',
+    UGU:'Cysteine',
+    UGC:'Cysteine',
+    UGG:'Tryptophan',
+    UAA:'STOP',
+    UAG:'STOP',
+    UGA:'STOP',
+  };
+
+  let proteins = [];
+
+  if (rnaSequence.length % 3 !== 0) {
+    throw Error('Invalid codon')
+  }
+
+  for (let i = 0; i < rnaSequence.length - 2; i += 3) {
+    let codon = rnaSequence.slice(i, i + 3);
+    
+    if (!(codon in codonMap)) {
+      throw Error('Invalid codon');
+    }
+
+    if (codonMap[codon] === 'STOP') {
+      break;
+    }
+
+    proteins.push(codonMap[codon]);
+  }
+
+  return proteins;
 };
