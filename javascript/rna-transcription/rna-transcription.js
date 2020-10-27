aa3b0d017f9e44b9ec47dec06b886a1523e1d6ae
jjkeyser <jakob.keyser@gmail.com>
2020-10-27 06:20:04 -0600

    Refactored to use array methods Refactored to remove the for loop and instead split the string into an array and used the reduce method to return the rna value of the dna character

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,11 +1,5 @@
 
 export const toRna = (dna) => {
-  let rna = '';
   let dnaMap = {G:'C', C:'G', T:'A', A:'U'};
-
-  for (let i = 0; i < dna.length; i++) {
-      rna += dnaMap[dna[i]];
-  }
-
-  return rna;
+  return dna.split('').reduce((a,b)=> a += dnaMap[b], '')
 };
