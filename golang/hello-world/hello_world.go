2de6a2f2ef214078638ad24cdf320f55c19112a2
jjkeyser <jakob.keyser@gmail.com>
2020-11-04 17:04:11 -0700

    first go exercise

---
+++ golang/hello-world/hello_world.go
@@ -0,0 +1,10 @@
+/*
+Package greeting is a standard Hello World exercise to get
+started using the go language.
+*/
+package greeting
+
+// HelloWorld should have a comment documenting it.
+func HelloWorld() string {
+	return "Hello, World!"
+}
