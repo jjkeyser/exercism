591af5cd8a628fa8d8366c693a67d909d9306650
jjkeyser <jakob.keyser@gmail.com>
2020-11-09 15:49:37 -0700

    passing all tests

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
+	var distance int
+	if len(a) != len(b) {
+		return distance, errors.New("Strings are different lengths, can't determine Hamming distance")
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+	return distance, nil
 }
