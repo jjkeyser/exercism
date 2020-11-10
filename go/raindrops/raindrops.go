0b6e1671d4642e9a35cf88b72d4b6c4af30931c7
jjkeyser <jakob.keyser@gmail.com>
2020-11-10 06:59:37 -0700

    passing all tests

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,19 @@
+package raindrops
+
+import "strconv"
+
+func Convert(input int) (result string) {
+	if input%3 == 0 {
+		result += "Pling"
+	}
+	if input%5 == 0 {
+		result += "Plang"
+	}
+	if input%7 == 0 {
+		result += "Plong"
+	}
+	if input%7 != 0 && input%5 != 0 && input%3 != 0 {
+		result = strconv.Itoa(input)
+	}
+	return result
+}
