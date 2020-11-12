032ac0819cb674d52524b8dbce9890b1793d6b9f
jjkeyser <jakob.keyser@gmail.com>
2020-11-12 06:47:44 -0700

    formatted replacer for readability

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -3,6 +3,11 @@ package strand
 import "strings"
 
 func ToRNA(dna string) string {
-	rna := strings.NewReplacer("G", "C", "C", "G", "T", "A", "A", "U")
+	rna := strings.NewReplacer(
+		"G", "C",
+		"C", "G",
+		"T", "A",
+		"A", "U",
+	)
 	return rna.Replace(dna)
 }
