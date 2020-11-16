76f0e86ee8e63edbf445c8c08579752ddd86ae7c
jjkeyser <jakob.keyser@gmail.com>
2020-11-16 15:46:12 -0700

    refactored to use switch statement

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -2,35 +2,29 @@ package dna
 
 import (
 	"fmt"
-	"strings"
 )
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
 type Histogram map[rune]int
 
-// DNA is a list of nucleotides. Choose a suitable data type.
+// DNA is a list of nucleotides
 type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
-// Counts is a method on the DNA type. A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-// Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram = Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
-	var err error
-	for _, n := range d {
-		_, ok := h[n]
-		if ok {
-			continue
-		} else {
-			err = fmt.Errorf("%q is not a valid nucleotide", n)
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	for _, nucleotide := range d {
+		switch nucleotide {
+		case
+			'A',
+			'C',
+			'G',
+			'T':
+			h[nucleotide]++
+		default:
+			return h, fmt.Errorf("%q is not a valid nucleotide", nucleotide)
 		}
 	}
-	for k := range h {
-		h[k] = strings.Count(string(d), string(k))
-	}
-	return h, err
+	return h, nil
 }
