1e217f1228bcbebbb8c427e5bb80268ebb985592
jjkeyser <jakob.keyser@gmail.com>
2020-11-16 14:03:19 -0700

    passing all tests

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        bool hasLetters = statement.Any(char.IsLetter);
+        bool hasLowerCaseLetters = statement.Any(char.IsLower);
+
+        bool isQuestion = statement.TrimEnd().EndsWith('?');
+        bool isEmpty = String.IsNullOrWhiteSpace(statement);
+
+        bool isShouting = hasLetters && !hasLowerCaseLetters;
+
+        string response = "Whatever.";
+
+        if (isEmpty) 
+        {
+            response = "Fine. Be that way!";
+        } 
+        else if (isQuestion && isShouting)
+        {
+            response = "Calm down, I know what I'm doing!";
+        }
+        else if (isQuestion)
+        {
+            response = "Sure.";
+        }
+        else if (isShouting)
+        {
+            response = "Whoa, chill out!";
+        }
+        return response;
     }
 }
