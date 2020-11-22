cb0c5dadf44d7d8f4e2071a2a757d38a7e15cf6c
jjkeyser <jakob.keyser@gmail.com>
2020-11-22 14:16:58 -0700

    switch statement is nearly 5x faster than looping a map

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -4,21 +4,32 @@ import (
 	"strings"
 )
 
-var scores = map[string]int{
-	"A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1, "S": 1, "T": 1,
-	"D": 2, "G": 2,
-	"B": 3, "C": 3, "M": 3, "P": 3,
-	"F": 4, "H": 4, "V": 4, "W": 4, "Y": 4,
-	"K": 5,
-	"J": 8, "X": 8,
-	"Q": 10, "Z": 10,
+func letterValue(r rune) int {
+	switch r {
+	case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+		return 1
+	case 'd', 'g':
+		return 2
+	case 'b', 'c', 'm', 'p':
+		return 3
+	case 'f', 'h', 'v', 'w', 'y':
+		return 4
+	case 'k':
+		return 5
+	case 'j', 'x':
+		return 8
+	case 'q', 'z':
+		return 10
+	default:
+		return 0
+	}
 }
 
 // Score returns the scrabble score of a word
 func Score(word string) int {
 	score := 0
-	for _, letter := range strings.ToUpper(word) {
-		score += scores[string(letter)]
+	for _, letter := range strings.ToLower(word) {
+		score += letterValue(letter)
 	}
 	return score
 }
