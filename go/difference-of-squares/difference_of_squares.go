11c3b7de1ac458fb7193917d144323975f683b79
jjkeyser <jakob.keyser@gmail.com>
2020-11-22 16:29:41 -0700

    used math to improve speed linearly

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,21 +1,20 @@
 package diffsquares
 
+// SquareOfSum returns the square of the sum of natural numbers from 1
+// up to a given number
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	sum := n * (n + 1) / 2
 	return sum * sum
 }
 
+// SumOfSquares returns the sum of the squares of natural numbers from 1
+// up to the give number
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 1; i <= n; i++ {
-		sum += i * i
-	}
-	return sum
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
+// Difference returns the difference between the square of the sum of a number
+// and the sum of the squares of a number
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
