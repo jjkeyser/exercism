919093868d0cca2a13078bf8370c1aa36e8c89f7
jjkeyser <jakob.keyser@gmail.com>
2020-11-27 10:07:08 -0700

    added comments to functions

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,22 +2,20 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
+const totalGrains = 1<<64 - 1
+
+// Square returns 2 raised to the power of the input minus 1
+// Returns an error if the input is < 1 or > 64
 func Square(input int) (uint64, error) {
-	var err error
 	if input < 1 || input > 64 {
-		err = errors.New("Input must be between 1 and 64")
+		return 0, errors.New("Input must be between 1 and 64")
 	}
-	return uint64(math.Pow(2, float64(input)-1)), err
+	return 1 << (input - 1), nil
 }
 
+// Total returns the max value of a uint64
 func Total() uint64 {
-	var totalGrains uint64
-	for i := 1; i <= 64; i++ {
-		square, _ := Square(i)
-		totalGrains += square
-	}
 	return totalGrains
 }
